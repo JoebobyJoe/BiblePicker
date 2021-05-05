982f91b81241b99dbb9bd3ea25d7772263c55fc1
joebobyjoe <59772194+joeboby@users.noreply.github.com>
2021-05-05 10:32:14 -0500

    Adding the project
    Adding the project to Github

---
+++ BiblePicker/app/src/main/java/com/example/biblepicker/FullscreenActivity.java
@@ -0,0 +1,272 @@
+package com.example.biblepicker;
+
+import android.annotation.SuppressLint;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+
+
+    //the file that stores the books
+    public static final String BOOK_LIST = "BookList";
+
+    private static final ArrayList<String> books = new ArrayList<>(Arrays.asList("Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles",
+            "Ezra", "Nehemiah", "Esther", "Job", "Psalms", "Proverbs", "Ecclesiastes", "Songs of Solomon", "Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Daniel", "Hosea", "Joel", "Amos",
+            "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi",
+            /*NEW TESTEMENT BOOKS*/
+            "Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians",
+            "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude", "Revelation"));
+
+
+
+    ArrayList<String> booksShuffled = null;
+    boolean isShuffled = false;
+
+
+
+
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private static final int UI_ANIMATION_DELAY = 300;
+    private final Handler mHideHandler = new Handler();
+    private View mContentView;
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            // Delayed removal of status and navigation bar
+
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    };
+    private View mControlsView;
+    private final Runnable mShowPart2Runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Delayed display of UI elements
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+            mControlsView.setVisibility(View.VISIBLE);
+        }
+    };
+    private boolean mVisible;
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hide();
+        }
+    };
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fullscreen);
+
+        mVisible = true;
+        mControlsView = findViewById(R.id.fullscreen_content_controls);
+        mContentView = findViewById(R.id.fullscreen_content);
+
+
+        // Set up the user interaction to manually show or hide the system UI.
+        mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggle();
+            }
+        });
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+
+
+        //get the stored data back since we just launched
+        SharedPreferences booksState = getSharedPreferences(BOOK_LIST,0);
+        Set<String> storedBookOrder = booksState.getStringSet("books", null);
+        isShuffled = booksState.getBoolean("shuffled", false);
+
+        //check to make sure the data we got back was vaild
+        if(storedBookOrder == null)
+            booksShuffled = new ArrayList<>(books);
+        else
+            booksShuffled = new ArrayList<>(storedBookOrder);
+
+        TextView output = findViewById(R.id.DiscriptionBox);
+        //check if the list is sorted, can print it out right aways if it is
+        if(isShuffled){
+            //list is sorted print out the books in order to read
+            System.out.println(booksShuffled.toString());
+            output.setText(booksShuffled.toString());
+        }
+    }
+
+    /**
+     * My function for the button that picks a new book of the bible
+     * */
+    public void pickBookButton(View view){
+
+        TextView output = findViewById(R.id.DiscriptionBox);
+
+        //pick a random number
+        Random rand = new Random();
+
+        //check if the list is sorted
+        if(isShuffled){
+            //list is sorted print out the books in order to read
+            output.setText(booksShuffled.toString());
+        }
+        else {
+            //list is not shuffled and should be shuffled, so do that
+            booksShuffled = shuffleList(books);
+
+            //print it out
+            output.setText(booksShuffled.toString());
+
+            //prepare stuff so that we are able to store the book list in its shuffled order
+            SharedPreferences booksState = getSharedPreferences(BOOK_LIST,0);
+            SharedPreferences.Editor editor = booksState.edit();
+
+            //store the settings
+            editor.putStringSet("books",  booksShuffled.stream().collect(Collectors.<String>toSet()));
+            editor.putBoolean("shuffled", true);
+            editor.commit();
+
+        }
+
+    }
+
+    /**
+     * shuffles a list of strings
+     *
+     * @param theBooks the list that will be shuffled
+     * @return a COPY of the given list in a shuffled order
+     * */
+    private ArrayList<String> shuffleList(ArrayList<String> theBooks){
+
+        Random rand = new Random();
+
+        ArrayList<String> shuffledList = new ArrayList<>(theBooks);
+        for (int i = 0; i < shuffledList.size(); i++) {
+            int randomIndexToSwap = rand.nextInt(shuffledList.size());
+            String temp = shuffledList.get(randomIndexToSwap);
+            shuffledList.set(randomIndexToSwap, shuffledList.get(i));
+            shuffledList.set(i, temp);
+        }
+
+        return shuffledList;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+    private void toggle() {
+        if (mVisible) {
+            hide();
+        } else {
+            show();
+        }
+    }
+
+    private void hide() {
+        // Hide UI first
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        mControlsView.setVisibility(View.GONE);
+        mVisible = false;
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable);
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    @SuppressLint("InlinedApi")
+    private void show() {
+        // Show the system bar
+        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        mVisible = true;
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable);
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    /**
+     * Schedules a call to hide() in delay milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+}
